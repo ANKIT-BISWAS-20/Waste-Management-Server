fe2afa8839e9b4c749b300473afa2fb014296776
Ankit <ankit_b.cse2021@msit.edu.in>
2024-07-12 23:13:24 +0530

    feat: Add pickup model
    This commit adds the pickup model in the `pickup.model.js` file. It defines the schema for the pickup collection in the MongoDB database, including fields for classname, thumbnail, owner, acceptedPrice, status, location, customerPrice, pickerPrice, paymentDone, and paymentId. It also includes timestamps for tracking creation and modification dates.

---
+++ src/models/pickup.model.js
@@ -0,0 +1,51 @@
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const pickupSchema = new Schema(
+    {
+        classname: {
+            type: String,
+            required: true,
+            index: true
+        },
+        thumbnail: {
+            type: String, 
+            required: true
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        },
+        acceptedPrice: {
+            type: Number,
+            default: 0
+        },
+        status: {
+            type: String,
+            default: "pending"
+        },
+        location: {
+            type: [Number],
+        },
+        customerPrice: {
+            type: [Number],
+        },
+        pickerPrice: {
+            type: [Number],
+        },
+        paymentDone: {
+            type: Boolean,
+            default: false
+        },
+        paymentId: {
+            type: String
+        },
+    }, 
+    {
+        timestamps: true
+    }
+)
+
+pickupSchema.plugin(mongooseAggregatePaginate)
+
+export const Pickup = mongoose.model("Pickup", pickupSchema)
