3fd08b0a93d9afa9b60e7318933ffe47173eac61
Ankit <ankit_b.cse2021@msit.edu.in>
2024-07-12 21:00:52 +0530

    feat: Add MongoDB connection configuration
    This commit adds the necessary configuration for connecting to a MongoDB database in the `db/index.js` file. It imports the `mongoose` package, defines the database name, and establishes a connection using the provided MongoDB URI. If the connection is successful, a success message is logged; otherwise, an error message is logged and the process exits.
    
    The purpose of this commit is to set up the MongoDB connection for the application.

---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const DB_NAME = "waste-management"
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}${DB_NAME}`)
+        console.log("Database Connected Successfully !!");
+    } catch (error) {
+        console.log("Database connection failed !! ", error);
+        process.exit(1)
+    }
+}
+
+export default connectDB
