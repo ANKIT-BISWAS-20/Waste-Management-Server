a0f9d4c55ff2795cfa03764dfbf3fac7eff5df68
Ankit <ankit_b.cse2021@msit.edu.in>
2024-07-12 23:39:23 +0530

    feat: Add generateAccessAndRefereshTokens function
    This commit adds the `generateAccessAndRefereshTokens` function to the `user.controller.js` file. The function generates access and refresh tokens for a given user ID. It retrieves the user from the database, generates the tokens, and updates the user's refresh token. This change enables token generation for authentication purposes.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -11,3 +11,20 @@ import { sendEmail } from "../utils/sendMail.js";
 dotenv.config({
     path: './.env'
 })
+
+const generateAccessAndRefereshTokens = async(userId) =>{
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        await user.save({ validateBeforeSave: false })
+
+        return {accessToken, refreshToken}
+
+
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating referesh and access token")
+    }
+}
