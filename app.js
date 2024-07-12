f02519769d9dfb19ed6b30e9ab97ae74e70db58c
Ankit <ankit_b.cse2021@msit.edu.in>
2024-07-12 22:26:38 +0530

    feat: Add server status route
    This commit adds a new route `/` for server status in the `serverStatus.routes.js` file. It imports the `serverStatus` controller function and sets up the route to handle GET requests. The purpose of this commit is to provide a route that returns a simple message indicating that the server is running.

--- app.js
+++ app.js
@@ -11,5 +11,8 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 
+// Server Status
+import serverStatusRouter from "./src/routes/serverStatus.routes.js"
+app.use("/",serverStatusRouter);
 
 export { app }
